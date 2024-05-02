9bcff158643a4aaaa907bc9ea7824de6c427f1b4
Max Bonetti <maxwell.bonetti@gmail.com>
2024-05-02 15:22:26 -0500

    post function read/write to file done for APIroutes

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,5 +1,4 @@
 //~~~~~ Setting paths for notes endpoint and all other endpoints ~~~~~//
-const path = require('path');
 const router = require('express').Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
@@ -10,6 +9,20 @@ router.get('/notes', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+//~~~~~POST function w/ READ and WRITE to file~~~~~//
+router.post('/notes', (req, res) => {
+    const newNote = { ...req.body, id: uuidv4() };
 
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    });
+});
 
 module.exports = router;
