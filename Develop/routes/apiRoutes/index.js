c1f1d1f2c46068a8bac08789d7bf31fde107b4da
Max Bonetti <maxwell.bonetti@gmail.com>
2024-05-02 19:01:14 -0500

    added delete function, fixed apiRoutes path

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -4,9 +4,18 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
-    fs.readFile('./db./db.json', 'utf8', (err, data) => {
-        if (err) throw err;
-        res.json(JSON.parse(data));
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Failed to read file:", err);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+        try {
+            const notes = JSON.parse(data);
+            res.json(notes);
+        } catch (parseErr) {
+            console.error("Error parsing JSON:", parseErr);
+            return res.status(500).json({ error: "Error parsing data" });
+        }
     });
 });
 //~~~~~POST function w/ READ and WRITE to file~~~~~//
@@ -21,6 +30,29 @@ router.post('/notes', (req, res) => {
         fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
             if (err) throw err;
             res.json(newNote);
+        });
+    });
+});
+
+//~~~~~DELETE function~~~~~//
+router.delete('/notes', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Failed to read file:', err);
+            return res.status(500).json({ error: "Internal server error occured" });
+        }
+        // Remove the note with the given ID
+        let notes = JSON.parse(data);
+        notes = notes.filter(note => note.id !== noteId);
+
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            if (err) {
+                console.error(`Failed to write to file: ${err}`);
+                return res.status(500).json({ error: "Internal server error occured" });
+            }
+            res.json({ message: "Note Deleted" });
         });
     });
 });
